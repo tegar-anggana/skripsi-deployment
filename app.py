f5be7873786ce4c7906b58dec87e309cbeac0b35
Tegar Levta Anggana <tegarla123@gmail.com>
2024-07-21 00:42:00 +0700

    progress

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-# pip install flask transformers
+# pip install flask transformers pandas
 # pip3 install torch 
 # torchvision torchaudio
+# labeling ipynb : https://drive.google.com/drive/u/0/folders/16SWta0z2NsFVLwYNLcV1dKukvYnhDxAv
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -19,6 +21,32 @@ def predict():
         text = request.form['text']
         result = classifier(text)
         return jsonify(result)
+    
+@app.route('/predict-file', methods=['POST'])
+def predict_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file and file.filename.endswith('.xlsx'):
+        # Read the file into a Pandas DataFrame
+        df = pd.read_excel(file, engine='openpyxl')
+        
+        columns = df.columns.tolist()
+        data = df.head().to_dict(orient='records')
+        
+        # Filter out NaN values
+        for row in data:
+            for key, value in row.items():
+                if pd.isna(value):
+                    row[key] = ""  # or any default value
+        
+        return jsonify({'columns': columns,'example_data': data}), 200
+    else:
+        return jsonify({'error': 'Invalid file format'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
